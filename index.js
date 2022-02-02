a1e5a912d4f1e1b5f5d8ba9fca1834edd2bcc052
Angelo Milien <angelomilienjoseph@gmail.com>
2022-02-02 10:55:50 -0500

    testing

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+function createEmployeeRecord(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+function createTimeInEvent(employee, dateStamp){
+    // let [date, hour] = dateStamp.split(' ')
+
+    // employee.timeInEvents.push({
+    //     type: "TimeIn",
+    //     hour: parseInt(hour, 10),
+    //     date,
+    // })
+
+    // return employee
+    console.log(dateStamp)
+}
