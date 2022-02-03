cfa6e821632d2bcc0102eb740a5181413b9fdfa0
Angelo <angelomilienjoseph@gmail.com>
2022-02-03 09:30:52 -0500

    commit tree 125e9860d79a9b34c05bb28b12eed21f82d66668

--- index.js
+++ index.js
@@ -18,14 +18,66 @@ function createEmployeeRecords(employeeRowData) {
 }
 
 function createTimeInEvent(employee, dateStamp){
-    // let [date, hour] = dateStamp.split(' ')
+    let [date, hour] = dateStamp.split(' ')
 
-    // employee.timeInEvents.push({
-    //     type: "TimeIn",
-    //     hour: parseInt(hour, 10),
-    //     date,
-    // })
-
-    // return employee
-    console.log(dateStamp)
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: Number(hour),
+        date: date
+    })
+    return employee
 }
+
+function createTimeOutEvent(employee, dateStamp){
+    const [date, hour] = dateStamp.split(' ')
+
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: Number(hour),
+        date: date
+    })
+    return employee
+}
+
+  
+function hoursWorkedOnDate(employee, dateStamp){
+    return (employee.timeOutEvents[0].hour - employee.timeInEvents[0].hour)/100
+}
+
+
+// {
+//     firstName: 'Julius',
+//     familyName: 'Caesar',
+//     title: 'General',
+//     payPerHour: 1000,
+//     timeInEvents: [ { type: 'TimeIn', hour: 900, date: '0044-03-15' } ],
+//     timeOutEvents: [ { type: 'TimeOut', hour: 1100, date: '0044-03-15' } ]
+//   }
+
+function wagesEarnedOnDate(employee, dateStamp){
+    
+    const hour = hoursWorkedOnDate(employee, dateStamp)
+     const salaryOwned = hour * employee.payPerHour
+    return salaryOwned
+}
+
+
+function allWagesFor(employee){
+
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+
+}
+
+function calculatePayrolls(employees){
+
+}
+
+// function allWagesFor(employee){
+
+// }
+
+// function allWagesFor(employee){
+
+// }
